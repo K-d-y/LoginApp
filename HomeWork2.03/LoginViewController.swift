0a716ccc0f2d4600f1b4ddb18cdde8cd7f5a3bcb
Dmitry Knauer <knauerdm@mail.ru>
2022-03-09 19:38:13 +0700

    Set constraints for WelcomeView

---
+++ HomeWork2.03/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  HomeWork2.03
+//
+//  Created by Dmitry Knauer on 09.03.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
--- HomeWork2.03/WelcomeViewController.swift
+++ HomeWork2.03/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  HomeWork2.03
 //
 //  Created by Dmitry Knauer on 09.03.2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
 }
-
